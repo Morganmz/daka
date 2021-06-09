f1d91b1d23ef0296c6019fb09876edb3b515517e
Guy <49865330+qianmz1718@users.noreply.github.com>
2021-06-09 21:10:59 +0800

    Update user.py

--- user.py
+++ user.py
@@ -12,7 +12,7 @@ def punchcard(username, password):
         'fieldSQbj': '24',
         'fieldSQxq': '2',
         'fieldSQgyl': '28',
-        'fieldSQqsh': '2225'
+        'fieldSQqsh': '225'
     }
 
     conditions = {
